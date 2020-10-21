34d01f7b3384f12597b402dd83f5000d31ddf85e
edanurakbass <edanurakbas6@gmail.com>
2020-10-21 16:13:51 +0300

    master

---
+++ Assets/Scriptes/GameManager.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class GameManager : MonoBehaviour
+{
+    public Soru[] sorular;
+    private static List<Soru> cevaplanmamisSorular;
+
+    private Soru gecerliSoru;
+
+    [SerializeField]
+    private Text soruText;
+
+    [SerializeField]
+    private Text dogruCvpTxt, yanlisCvpTxt;
+
+    [SerializeField]
+    private GameObject dogruBtn, yanlisBtn;
+
+    int dogruAdet, yanlisAdet;
+    int toplamPuan;
+
+    [SerializeField]
+    private GameObject sonucPaneli;
+
+    SonucManager sonucManager;
+
+    void Start()
+    {
+        if (cevaplanmamisSorular == null || cevaplanmamisSorular.Count == 0)
+        {
+            cevaplanmamisSorular = sorular.ToList<Soru>();
+        }
+        yanlisAdet = 0;
+        dogruAdet = 0;
+        toplamPuan = 0;
+
+        RastgeleSoruSec();
+        
+    }
+
+    void RastgeleSoruSec()
+    {
+        yanlisBtn.GetComponent<RectTransform>().DOLocalMoveX(320f, .2f);
+        dogruBtn.GetComponent<RectTransform>().DOLocalMoveX(-320f, .2f);
+        int randomSoruIndex = Random.Range(0, cevaplanmamisSorular.Count);
+        gecerliSoru = cevaplanmamisSorular[randomSoruIndex];
+
+        soruText.text = gecerliSoru.soru;
+
+        if (gecerliSoru.dogruMu)
+        {
+            dogruCvpTxt.text = "DOGRU CEVAPLADINIZ";
+            yanlisCvpTxt.text = "YANLIS CEVAPLADINIZ";
+        }
+        else
+        {
+            dogruCvpTxt.text = "YANLIS CEVAPLADINIZ";
+            yanlisCvpTxt.text = "DOGRU CEVAPLADINIZ";
+        }
+    }
+
+    IEnumerator SorularArasiBekleRoutine()
+    {
+        cevaplanmamisSorular.Remove(gecerliSoru);
+        yield return new WaitForSeconds(1f);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+        if (cevaplanmamisSorular.Count <= 0)
+        {
+            sonucPaneli.SetActive(true);
+
+            sonucManager = Object.FindObjectOfType<SonucManager>();
+            sonucManager.SonuclariYAzdir(dogruAdet, yanlisAdet, toplamPuan);
+        }
+        else
+        {
+            RastgeleSoruSec();
+        }
+
+    }
+    public void DogruButonBasildi()
+    {
+        if (gecerliSoru.dogruMu)
+        {
+            dogruAdet++;
+            toplamPuan += 100;
+        }
+        else
+        {
+            yanlisAdet++;
+        }
+        yanlisBtn.GetComponent<RectTransform>().DOLocalMoveX(1000f,.3f);
+        StartCoroutine(SorularArasiBekleRoutine());
+    }
+
+    public void YanlisButonBasildi()
+    {
+        if (!gecerliSoru.dogruMu)
+        {
+           dogruAdet++;
+            toplamPuan += 100;
+        }
+        else
+        {
+            yanlisAdet++;
+        }
+
+        dogruBtn.GetComponent<RectTransform>().DOLocalMoveX(-1000f,.3f);
+        StartCoroutine(SorularArasiBekleRoutine());
+    }
+    
+}
---
+++ Assets/Scriptes/SonucManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SonucManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text dogruTxt, yanlisTxt, puanTxt;
+
+    [SerializeField]
+    private GameObject solYildiz, ortaYildiz, sagYildiz;
+    public void SonuclariYAzdir( int dogruAdet, int yanlisAdet, int puan)
+    {
+        dogruTxt.text = dogruAdet.ToString();
+        yanlisTxt.text = yanlisAdet.ToString();
+        puanTxt.text = puan.ToString();
+
+        solYildiz.SetActive(false);
+        ortaYildiz.SetActive(false);
+        sagYildiz.SetActive(false);
+
+        if (dogruAdet == 1)
+        {
+            solYildiz.SetActive(true);
+        }
+        else if (dogruAdet == 2)
+        {
+            solYildiz.SetActive(true);
+            ortaYildiz.SetActive(true);
+        }
+        else
+        {
+            solYildiz.SetActive(true);
+            ortaYildiz.SetActive(true);
+            sagYildiz.SetActive(false);
+        }
+    }
+}
